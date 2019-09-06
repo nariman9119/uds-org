51e849f70f87f404c89d9ab23ac20de8b6aad75f
nariman9119 <nariman9119@gmail.com>
2019-09-07 02:50:16 +0300

    seems to be work

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App.js";
+import MyComponent from './components/tempComponent';
+
+export{
+    App
+}
+
 
 ReactDOM.render(<App />, document.getElementById("root"));
