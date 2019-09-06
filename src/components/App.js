b1d279961bd2d502027b73cea468cdcd27d244d4
nariman9119 <nariman9119@gmail.com>
2019-09-07 00:53:59 +0300

    First commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import '../styles/App.css';
+import React, {Component} from 'react';
+
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>UDS-ORG</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
